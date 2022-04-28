3ca46f6287bae446280b486ff9e4dd3ae22c967b
Victor Lobe <32442832+victorlobe@users.noreply.github.com>
2022-04-28 03:03:19 +0200

    Swift Package

--- Package.swift
+++ Package.swift
@@ -2,24 +2,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Postal",
-    platforms: [
-        .iOS(.v12),
-    ],
-    products: [
-        .library(
-            name: "Postal",
-            targets: ["Postal", "Postal.iOS", "Postal-macOS"]),
-    ],
-    dependencies: [
-        // no dependencies
-    ],
-    targets: [
-        .target(
-            name: "Postal",
-            dependencies: ["Postal", "Postal.iOS", "Postal-macOS"]),
-        .testTarget(
-            name: "PostalTests",
-            dependencies: ["Postal", "Postal.iOS", "Postal-macOS"]),
-    ]
-)
+  name: "Postal",
+  platforms: [.iOS("11.0"), .macOS("10.10"), .tvOS("11.0")],
+  products: [.library(name: "Postal", targets: ["Postal"])],
+  targets: [.target(name: "Postal", path: "Sources")])
